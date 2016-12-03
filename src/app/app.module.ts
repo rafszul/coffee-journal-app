3f179e403e3dbe12835d07fd13d333cea84360d9
rafszul <rafszul@gmail.com>
2016-12-03 19:36:17 +0000

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import 'hammerjs';
+
+import { MaterialModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +16,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    [MaterialModule.forRoot()]
   ],
   providers: [],
   bootstrap: [AppComponent]
