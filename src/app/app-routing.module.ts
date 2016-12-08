bad676c8595fb8ce46f399df6d5d2ebf7a7e13bb
@rafszul <rafszul@gmail.com>
2016-12-08 08:39:21 +0000

    some fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-//import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-//import { ContactComponent } from './contact/contact.component';
-//import { ChatComponent } from './chat/chat.component';
+// import { ContactComponent } from './contact/contact.component';
+// import { ChatComponent } from './chat/chat.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    //{ path: 'contact', component: ContactComponent },
-    //{ path: 'chat', component: ChatComponent },
-    //{ path: '**', component: LostComponent }
+    // { path: 'contact', component: ContactComponent },
+    // { path: 'chat', component: ChatComponent },
+    // { path: '**', component: LostComponent }
 ];
 
 @NgModule({
