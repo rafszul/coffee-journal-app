9aebd59f4a077f1d0627f76f6b707f7f7749f83d
@rafszul <rafszul@gmail.com>
2017-01-04 06:22:45 +0000

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,16 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { ChatComponent } from './chat/chat.component';
 import { LostComponent } from './lost/lost.component';
+import { EspressoJournalComponent } from './espresso-journal/espresso-journal.component';
+import { FilterJournalComponent } from './filter-journal/filter-journal.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'chat', component: ChatComponent },
+    { path: 'espresso-journal', component: EspressoJournalComponent },
+    { path: 'filter-journal', component: FilterJournalComponent },
     { path: '**', component: LostComponent }
 ];
 
