cea5bcba738be1655ef2827ff74e985dc6d6aed2
rafszul <rafszul@gmail.com>
2017-01-05 08:11:00 +0000

    firebase connect

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  items: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/items');
+  }
 
   ngOnInit() {
   }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAhxWY0zQpZr8_4plD0b3SGW6gbvjUQT9U',
+  authDomain: 'coffee-journal-app.firebaseapp.com',
+  databaseURL: 'https://coffee-journal-app.firebaseio.com',
+  storageBucket: 'coffee-journal-app.appspot.com',
+  messagingSenderId: '604494717959'
+};
